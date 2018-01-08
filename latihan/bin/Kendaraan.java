4661365005bdbc5bcf1c4da84d7f1d745df1d998
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-08 14:11:24 +0700

    Contruktor java

--- latihan/bin/Kendaraan.java
+++ latihan/bin/Kendaraan.java
@@ -2,14 +2,29 @@ package latihan.bin;
 
 public class Kendaraan{
 
-	public Integer jumlahRoda=4;
+	private  Integer jumlahRoda=4;
 	private String pabrik;
 
-	public String getNamaPabrik(){
+	public Kendaraan(Integer jumlahRoda){
+       this.jumlahRoda = jumlahRoda;
+    }
+ 
+      // private Integer jumlahRoda = 4;
+      // private String pabrik;
+
+	public Integer getJumlahRoda(){
+		return this.jumlahRoda;
+	}
+
+	public void setJumlahRoda(Integer jumlahRoda){
+	this.jumlahRoda = jumlahRoda;
+}
+
+	public String getPabrik(){
 		return this.pabrik;
 	}
 
-	public void setNamaPabrik(String pabrik){
+	public void setPabrik(String pabrik){
 	this.pabrik = pabrik;
-}
+  }
 }
--- latihan/bin/Mobil.java
+++ latihan/bin/Mobil.java
@@ -4,28 +4,50 @@ public class Mobil extends Kendaraan{
 	private String nama;
 
 	public void setNama(String nama){
-	System.out.println(jumlahRoda);
+	System.out.println(super.getJumlahRoda());
 	this.nama=nama;
 }
+
+public Mobil(){
+        super(4);
+         System.out.println("ini contruktur di pangil ketika di inisialiasi");
+     }
+ 
+     public Mobil(String nama){
+         super(30);
+         this.nama = nama;
+         System.out.println(
+             String.format("variable nama berisi %s", this.nama)
+         );
+     }
+public String getPabrik(){
+	return super.getPabrik();
+}
+
+
+public void setNama(){
+	System.out.println(super.getJumlahRoda());
+	this.nama="Mitshubishi";
+}
+
 public String nama(){
 	return this.nama;
 }
-
-public void namaPabrik(){
-	System.out.println( String.format("nama pabrik %s",super.getNamaPabrik() ));
-}
+// public void namaPabrik(){
+// 	System.out.println( String.format("nama pabrik %s",super.getPabrik() ));
+// }
 
 
 
 public static void main(String[] args){
-	Mobil honda = new Mobil();
+	Mobil honda = new Mobil("Lambogin");
 	honda.setNama("Honda civic");
 	// honda.setNamaPabrik("Selow");
 	System.out.println(honda.nama());
 	honda.setNama("Honda Civic");
 	System.out.println(honda.nama());
-	honda.setNamaPabrik("Astra Honda Dalam");
-	honda.namaPabrik();
+	honda.setPabrik("Astra Honda Dalam");
+	// honda.namaPabrik();
 	
 }
 
